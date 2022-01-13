d5fabff803f618651f25974d619e11edd4c85c38
NHQuang58 <qn052289@gmail.com>
2022-01-13 13:10:44 +0700

    ok

--- src/app/redisConnect.js
+++ src/app/redisConnect.js
@@ -1,10 +1,27 @@
 const redis = require('redis');
+const Redis = require('ioredis')
 const { configRedis } = require('../config/configRedis');
 const logger = require('../config/logger');
+const url = require("url");
 
-const redisClient = redis.createClient({ host: configRedis.redisPort, port: configRedis.redisHost });
+let redisClient;
+if(process.env.NODE_ENV !== "production")
+{
+  redisClient = redis.createClient({ host: configRedis.redisPort, port: configRedis.redisHost });
+  redisClient.connect().then(() => logger.info('Connect to Redis Local'))
+}
 
-redisClient.connect().then(() =>{logger.info("Redis connect success!!!")} );
+if(process.env.NODE_ENV === "production") {
+  logger.info('Connect to Redis To go')
+  let rtg = url.parse(process.env.REDISTOGO_URL);
+  let pass = rtg.auth.split(":")[1];
+  redisClient = new Redis({
+    host: rtg.hostname,
+    port: rtg.port,
+    password: pass,//'327b42f444a57620625a31f9eb4df4b7'
+  });
+}
+
 redisClient.on('ready', function () {
   logger.info('Redis is ready');
 });
